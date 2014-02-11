0b286174f4c5c3e65c5666a116c98a0d5159d616
Andrey Arefiev <arefiev@gmail.com>
2014-02-12 01:18:50 +0400

    add secret_token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LamBlog::Application.config.secret_token = '7f00075bb1c6a82f3a6b50fe338c7b347b5be3941180ea2a3f9202b0b7f094936c8b6a6276d488950230d0f76eff7c92e1afa99d7c6281176563a2d0928afb0c'
